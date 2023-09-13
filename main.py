fa6367661c01f40538b0b12c89d95229de348816
Gissom Ting <grissom@htm.tw>
2023-09-13 10:49:16 +0800

    wip

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ def square(number):
     :param number: int or float
     :return: int or float, the square of the input number
     """
-def square(number):
-        square_number=number*number
-        return square_number
-number = int(input("Enter the number: "))
-I = square(number)
-print("The square of", number, "is:", I)
+    square_number = number * number
+    return square_number
 
 
 def reverse_string(s):
@@ -18,11 +14,7 @@ def reverse_string(s):
     :param s: str
     :return: str, the reversed string
     """
-def reverse_string(s):
-  return s[::-1]
-string = input("Enter a string:")
-reverse_string = reverse_string(string)
-print("Reversed string:", reverse_string)
+    return s[::-1]
 
 
 def factorial(n):
@@ -31,14 +23,19 @@ def factorial(n):
     :param n: int
     :return: int, the factorial of the input number
     """
+    result = 1 
+    for i in range(n,0,-1):
+         result *= i
+    return result
+
+
 def is_prime(n):
-    for i in range(2,n-1):
-      if n%i==0:
-         return False
+    for i in range(2, n - 1):
+        if n % i == 0:
+            return False
     else:
         return True
-a=int(input("Enter a number"))
-print(is_prime(a))
+
 
 def find_maximum(lst):
     """
@@ -46,14 +43,11 @@ def find_maximum(lst):
     :param lst: list of int
     :return: int, the maximum number in the list
     """
-def find_maximum(lst):
-    maximum=0
+    maximum = lst[0]
     for i in lst:
-        if i>maximum or i==maximum:
-            maximum=i
+        if i > maximum:
+            maximum = i
     return maximum
-a=[1,2,3,4,5,6,7,8]
-print(f"{find_maximum(a)}")
 
 
 def odd_or_even(n):
@@ -62,52 +56,39 @@ def odd_or_even(n):
     :param n: int
     :return: str, "Odd" or "Even"
     """
-def odd_or_even(n):
     if n % 2 == 0:
-        print("Yes, it is an even number")
+        return "Even"
     else:
-        print("It is an odd number")
+        return "Odd"
 
-a = int(input("Enter a number: "))
-odd_or_even(a)
-
-        
 
 def is_palindrome(s):
     """
-    This function takes a string `s` and returns `True` if the string is a palindrome, and `False` otherwise. 
+    This function takes a string `s` and returns `True` if the string is a palindrome, and `False` otherwise.
     A palindrome is a word or phrase that reads the same backward as forward.
-    
+
     :param s: str
     :return: bool, `True` if the string is a palindrome, `False` otherwise.
     """
-def is_palindrome(s):
     return s == s[::-1]
-print(is_palindrome("racecar")) 
-print(is_palindrome("hello"))    
-print(is_palindrome("level")) 
-
 
 
 def find_gcd(a, b):
     """
     This function takes two positive integers `a` and `b` and returns their greatest common divisor (GCD).
-    
+
     :param a: int
     :param b: int
     :return: int, the greatest common divisor of `a` and `b`.
     """
-    divisor=0
-    if a<b:
-        for i in range(1,a+1,1):
-            if a%i==0 and b%i==0:
-                divisor=i
-    if b<a:
-        for i in range(1,b+1,1):
-            if a%i==0 and b%i==0:
-                divisor=i   
-            
+    divisor = 0
+    if a < b:
+        for i in range(1, a + 1, 1):
+            if a % i == 0 and b % i == 0:
+                divisor = i
+    if b < a:
+        for i in range(1, b + 1, 1):
+            if a % i == 0 and b % i == 0:
+                divisor = i
+
     return divisor
-
-
-
